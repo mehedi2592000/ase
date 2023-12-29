f1bec81f5499afb40a50033643175d04c01f5f4d
mehedi <bd34017053@gmail.com>
2023-12-29 10:37:07 +0600

    Test-1 second commant

---
+++ MVC/MVC/Controllers/2ndController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class _2ndController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Test-1 second COntroller 
+    }
+}
