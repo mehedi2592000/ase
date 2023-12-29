9106bdf8a9f16026d320ba0b6e499159f16fa3ca
mehedi <bd34017053@gmail.com>
2023-12-29 10:23:40 +0600

    3rd commit

---
+++ MVC/MVC/Controllers/1StController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class _1StController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //1111111111111111111111111111111111111111111111k
+    }
+}
